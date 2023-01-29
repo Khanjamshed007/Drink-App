6acba8341449cc28533aaee5ae3adf13175a8e14
jamshed khan <khanjamshed404@gmail.com>
2023-01-29 20:17:18 +0530

    initial commited

---
+++ server.js
@@ -0,0 +1,21 @@
+const express=require('express');
+const app=express();
+const ejs=require('ejs');
+const path=require('path');
+const ejsLayout=require('express-ejs-layouts');
+const port=process.env.port || 8000;
+
+app.get('/',(req,res)=>{
+    res.render("home");
+})
+
+
+// Set the template engine
+app.use(ejsLayout);
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs');
+
+// Running the server on port
+app.listen(port,()=>{
+    console.log(`Listing on port ${port}`);
+})
